d25ca4467e30561a76995adc78ac53a35c5e5a4a
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-03-10 02:30:31 -0500

    commit tree da23fb7b78ab06fb112c7d5f265381f1ca62eeba

--- src/index.js
+++ src/index.js
@@ -13,3 +13,13 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+
+/* 
+Provider 
+Makes the Redux store available to the connect() calls in the component hierarchy below.
+
+createStore
+Creates a Redux store that holds the state tree. 
+The only way to change the data in the store is to call dispatch() on it.  There should only be a single store in your app. To specify how different parts of the state tree respond to actions, you may combine several reducers into a single reducer function by using combineReducers.
+*/
